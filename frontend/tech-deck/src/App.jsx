f1e8e6656811a06a33f8f53488d453e5bd15ed83
Siyi Ji <sylviajsyy@gmail.com>
2025-12-13 17:53:50 -0700

    Fixing dependency issue:

--- frontend/tech-deck/src/App.jsx
+++ frontend/tech-deck/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import FlashCard from './component/FlashCard.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [card, setCard] = useState([])
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/cards');
+        const data = await response.json();
+        console.log("API data:", data);
+        setCard(data);
+      } catch (error) {
+        console.warn(error);
+      }
+    }
+    fetchCards();
+  },[])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {card.map((singleCard) => (
+              <FlashCard 
+                key={singleCard.id} 
+                card={singleCard} 
+              />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
